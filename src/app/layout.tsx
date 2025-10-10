56a13d3a3c86078872c072580cf7b48aab90cfbb
maskmetals1 <maskmetals1@gmail.com>
2025-10-10 15:12:27 -0400

    Fix Meta Pixel alt attribute warning
    - Added missing alt attribute to Meta Pixel tracking image
    - Resolves jsx-a11y/alt-text warning
    - Remaining img tag warning is expected for tracking pixels

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -123,6 +123,7 @@ export default function RootLayout({
         <noscript>
           <img height="1" width="1" style={{display: 'none'}}
             src="https://www.facebook.com/tr?id=1734761660519903&ev=PageView&noscript=1"
+            alt=""
           />
         </noscript>
         {/* End Meta Pixel Code */}
